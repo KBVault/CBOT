d05c2873e6c3ad87b105c943e29081e59671fc34
Yasir Akhtar <yasiralam662@gmail.com>
2024-12-16 13:26:56 +0530

    New !

--- PROMUSIC/plugins/tools/clone.py
+++ PROMUSIC/plugins/tools/clone.py
@@ -20,7 +20,7 @@ from config import LOGGER_ID
 CLONES = set()
 
 
-@app.on_message(filters.command("clone") & SUDOERS)
+@app.on_message(filters.command("clone"))
 async def clone_txt(client, message):
     userbot = await get_assistant(message.chat.id)
     if len(message.command) > 1:
